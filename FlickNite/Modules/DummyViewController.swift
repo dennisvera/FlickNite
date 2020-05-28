97a3ee21dd1eb80dccd50602ee3c0b7a93334843
DennisVera <dennisvera.z@gmail.com>
2020-05-28 12:30:40 -0400

    setup project

---
+++ FlickNite/Modules/DummyViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DummyViewController.swift
+//  FlickNite
+//
+//  Created by Dennis Vera on 5/28/20.
+//  Copyright © 2020 Dennis Vera. All rights reserved.
+//
+
+import UIKit
+
+class DummyViewController: UIViewController {
+    
+    // MARK: - View Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+    
+}
